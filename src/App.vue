90647d330ff2c165f7146b815ccc7eff5eb5eed0
Jesse Hintze <jessehintze@Jesses-MacBook-Pro.local>
2026-07-08 10:13:52 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<script>
+import CharacterFacts from './components/CharacterFacts.vue'
+export default {
+  components: {
+    CharacterFacts,
+  },
+  data: () => ({
+    newCharacter: {
+      name: '',
+      drink: [],
+    },
+    characterList: [
+      {
+        name: 'The Dude',
+        drink: ['cosmo', 'white russian'],
+      },
+      {
+        name: 'Maude',
+        drink: ['cosmo', 'wine'],
+      },
+      {
+        name: 'Walter',
+        drink: ['soda', 'coffee', 'root beer'],
+      },
+      {
+        name: 'Donny',
+        drink: ['root beer', 'Mt. Dew'],
+      },
+    ],
+    favoriteCharacter: [],
+  }),
+
+  methods: {
+    addNewCharacter() {
+      this.characterList.push(this.newCharacter)
+      this.newCharacter = { name: '', drink: '' }
+    },
+    makeFavorite(character) {
+      this.favoriteCharacter.push(character)
+    },
+  },
+}
+</script>
+<CharacterFacts :characters="characterList" />
+<template>
+  <ul v-if="favoriteCharacter.length > 0">
+    <li v-for="(actor, index) in favoriteCharacter" :key="`actor-${index}`">
+      {{ actor.name }}
+    </li>
+  </ul>
+  <p v-else>There are no favorite Characters</p>
+</template>
